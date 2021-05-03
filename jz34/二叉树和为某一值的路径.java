318fd7a5334ecf921e5b9d3aa95bf364477a82f9
H48530 <485301267@qq.com>
2021-05-03 11:58:13 +0800

    二叉树和为某一值的路径

---
+++ jz34/二叉树和为某一值的路径.java
@@ -0,0 +1,47 @@
+package jz34;
+
+import java.util.*;
+
+
+/**
+ *
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class 二叉树和为某一值的路径 {
+    LinkedList<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        recur(root, target);
+        return res;
+    }
+
+    void recur(TreeNode root, int tar) {
+        if (root == null) return;
+        path.add(root.val);
+        tar -= root.val;
+        if (tar == 0 && root.left == null && root.right == null)
+            res.add(new LinkedList(path));
+        recur(root.left, tar);
+        recur(root.right, tar);
+        path.removeLast();
+    }
+}
