e28d03df2596a88be3777f3a60766d3f5aeaed45
H48530 <485301267@qq.com>
2021-05-09 22:40:15 +0800

    在排序数组中查找数字

---
+++ jz53/在排序数组中查找数字.java
@@ -0,0 +1,15 @@
+package jz53;
+
+/**
+ *
+ */
+public class 在排序数组中查找数字 {
+    public int search(int[] nums, int target) {
+
+    }
+
+
+    private int getFirst(int[]nums,int target,int from,int to ){
+
+    }
+}
