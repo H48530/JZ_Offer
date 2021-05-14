b474583e3114d73632e7f810b68b53f51df2d385
H48530 <485301267@qq.com>
2021-05-14 22:06:19 +0800

    不用加减乘除做加法

---
+++ jz65/不用加减乘除做加法.java
@@ -0,0 +1,16 @@
+package jz65;
+
+/**
+ *
+ */
+public class 不用加减乘除做加法 {
+    public int add(int a, int b) {
+        while (b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+
+}
