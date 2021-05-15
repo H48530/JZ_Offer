dcac6e67b9129249c68f876385e2bb1a46bb5854
H48530 <485301267@qq.com>
2021-05-15 21:26:45 +0800

    二叉树的最近公共祖先

---
+++ jz68/二叉树的最近公共祖先.java
@@ -0,0 +1,42 @@
+package jz68;
+
+/**
+ *
+ */
+public class 二叉树的最近公共祖先 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == p || root == q) {
+            return root;
+        }
+        boolean pindex = contains(root.left, p);
+        boolean qindex = contains(root.left, q);
+        if (pindex && !qindex) {
+            //p在左子树，q不在
+            return root;
+        }
+        if (qindex && !pindex) {
+            //q在左子树，p不在
+            return root;
+        }
+        if (pindex) {
+            //p、q在左子树，
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
+
+    public boolean contains(TreeNode root, TreeNode node) {
+        if (root == null) {
+            return false;
+        }
+        if (root == node) {
+            return true;
+        }
+        if (contains(root.left, node)) {
+            return true;
+        }
+        return contains(root.right, node);
+    }
+}
